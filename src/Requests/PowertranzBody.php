2df5bf7bb764ff39446f28c07219e7e0359d6bd1
hlca <hsinglic@gmail.com>
2023-09-25 18:48:40 -0600

    amount when building body

--- src/Requests/PowertranzBody.php
+++ src/Requests/PowertranzBody.php
@@ -8,7 +8,8 @@ class PowertranzBody
 {
     public static function fromCreditCard(PowertranzCreditCard $card,
         $transactionId = null,
-        $toVoid = false
+        $toVoid = false,
+        $amount = 5,
     ): array {
         if ($toVoid) {
             return self::voidPowertranzBody($transactionId, $card->id);
@@ -21,7 +22,8 @@ class PowertranzBody
             $card->cvv,
             $card->expiryDate,
             $card->name,
-            $card->billingAddress
+            $card->billingAddress,
+            $amount
         );
     }
 
@@ -83,11 +85,12 @@ class PowertranzBody
         string $cardCvv,
         string $cardExpiration,
         string $cardName,
-        string $billingAddress
+        string $billingAddress,
+        float $amount
     ): array {
         $body = [
             "TransactionIdentifier"  => $transactionId,
-            "TotalAmount"            => 5,
+            "TotalAmount"            => $amount,
             "CurrencyCode"           => "320",
             "Tokenize"               => true,
             "ThreeDSecure"           => true,
